6eb2e6c834ae39ccb66157871633d9624dbc4674
Sune Carelse <sune.carelse@takealot.com>
2021-01-15 23:20:03 +0200

    header created in storybook

---
+++ src/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className="header">
+        <div className="header__left">
+            <img
+                src="https://th.bing.com/th/id/OIP.88F_1Z5zGNZ-RIj3GfuPMAHaJl?pid=Api&rs=1"
+                alt=""
+            />
+        </div>
+        <div className="header__right">
+            <h2>Buttons will go here...</h2>
+        </div>
+    </div>
+  )
+};
+
+export default Header;
